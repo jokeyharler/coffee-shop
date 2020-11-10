3e780245727ab59f2ac322d70f36d058cedad01b
jokeyharler <jokeyharler@gmail.com>
2020-11-10 16:04:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/coffeeshoplayout/menuApp.java
@@ -0,0 +1,14 @@
+package com.example.coffeeshoplayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menuApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_app);
+    }
+}
